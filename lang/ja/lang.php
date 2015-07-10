b85b14e1334b06a6de07fff8a4712c225bc73bd8
Hideaki SAWADA <chuno@live.jp>
2015-07-10 14:26:35 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['approve'] = '承認する';
+$lang['approved'] = '承認済';
+$lang['draft'] = '草稿';
+$lang['last_approved'] = '最終承認';
+$lang['newest_draft'] = '最新草稿';
+$lang['newest_approved'] = '最新承認';
