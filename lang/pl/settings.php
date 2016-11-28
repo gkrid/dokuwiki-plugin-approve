dc2fc90d5308ddb76c89d201c2db2474e1d7b6f3
Szymon Olewniczak <(my first name)[at]imz.re>
2016-11-28 18:30:16 +0100

    fix

--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -1,4 +1,4 @@
 <?php
 $lang['no_apr_namespaces'] = 'Przestrzenie nazw dla których wtyczka <strong>nie będzie</strong> używana (rozdzielone spacjami).';
 
-$lang['prettyprint'] = 'Urzyj ładnych nagłówków przy wydruku.';
+$lang['prettyprint'] = 'Użyj ładnych nagłówków przy wydruku.';
