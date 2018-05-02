e973068339c931b3670ab8fe3d739b1f3072e3c9
Szymon Olewniczak <solewniczak@rid.pl>
2018-05-02 10:09:56 +0200

    add german translation. closes #8

---
+++ lang/de/settings.php
@@ -0,0 +1,3 @@
+<?php
+$lang['no_apr_namespaces'] = 'Namensräume, wo dieses Plugin <strong>NICHT</strong> gelten soll (mehrere mit Leerzeichen separieren)';
+$lang['prettyprint'] = 'Ausdruck mit schöner Überschrift';
