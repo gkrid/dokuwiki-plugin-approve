c9403cb0010f3b2f29ecc3d34b42a2639de200c8
Szymon Olewniczak <solewniczak@rid.pl>
2019-08-08 12:51:28 +0200

    migration fix

--- conf/default.php
+++ conf/default.php
@@ -1,4 +1,9 @@
 <?php
+$conf['apr_namespaces'] = '';
 $conf['no_apr_namespaces'] = '';
 $conf['prettyprint'] = 0;
 $conf['ready_for_approval'] = 0;
+
+$conf['sum approved'] = 'Approved';
+$conf['sum ready for approval'] = 'Ready for approval';
+$conf['sum draft'] = 'Draft';
