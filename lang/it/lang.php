171f38853532628ba046f852aeb816dd7f460226
Szymon Olewniczak <solewniczak@rid.pl>
2020-07-18 09:23:24 +0200

    italian translation

---
+++ lang/it/lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['approve'] = 'Approva';
+$lang['approved'] = 'Approvato';
+$lang['approve_ready'] = 'Segna pronto per approvazione';
+$lang['marked_approve_ready'] = 'Pronto per approvazione';
+$lang['approver'] = 'approver';
+
+$lang['draft'] = 'Bozza';
+$lang['last_approved'] = 'Ultima approvazione';
+$lang['newest_draft'] = 'Bozze recenti';
+$lang['newest_approved'] = 'Approvazioni recenti';
+
+$lang['hdr_page'] = 'Pagina';
+$lang['hdr_state'] = 'Stato';
+$lang['hdr_updated'] = 'Aggiornato';
+$lang['hdr_approver'] = $lang['approver'];
+
+$lang['all_approved'] = 'Approvato';
+$lang['all_approved_ready'] = 'Pronto per approvazione';
+
+$lang['by'] = 'da';
+
+$lang['version'] = 'versione';
+
+// menu entry for admin plugins
+$lang['menu'] = 'Assegnazione approvazione';
+
+$lang['admin btn_delete'] = 'Elimina';
+$lang['admin btn_add'] = 'Aggiungi';
+$lang['admin h_assignment_namespace'] = 'Pagina/Namespace';
+$lang['admin h_assignment_approver'] = $lang['approver'];
+
+$lang['notification full'] = 'Hai pagine da approvare: %s';
+
+$lang['js']['group_option_label'] = 'Gruppo:';
---
+++ lang/it/settings.php
@@ -0,0 +1,8 @@
+<?php
+$lang['no_apr_namespaces'] = 'Namespaces che questo plugin <strong>non</strong> si applica (lista separata da spazi).';
+$lang['prettyprint'] = 'Una intestazione bella.';
+$lang['ready_for_approval'] = 'Abilita stato intermedio "Imposta pronto per approvazione" per rendere noto al reviewer che è stato verificato/approvato';
+$lang['strict_approver'] = 'Solo gli approvers possono approvare le pagine.';
+$lang['hide_drafts_for_viewers'] = 'Nascondi bozze agli utenti senza permessi di editing e visualizza invece ultima versione approvata.';
+$lang['display_banner_for_readonly'] = 'Visualiuzza il banner per gli utenti con solo AUTH_READ.';
+$lang['banner_long'] = 'Banner lungo che include versione numero e approver';
